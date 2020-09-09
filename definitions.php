5e48cca99435319778cab3240247b0cb4b20f24c
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-09-09 11:36:20 +0200

    cleanup

--- definitions.php
+++ definitions.php
@@ -26,7 +26,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-    rldh: Rogier Lankhorst Definitions hyperlinks
+    Rogier Lankhorst Definitions
 */
 
 defined('ABSPATH') or die("you do not have acces to this page!");
--- text_parser.php
+++ text_parser.php
@@ -3,7 +3,7 @@ defined('ABSPATH') or die("you do not have acces to this page!");
 
 if (!class_exists('wpdef_text_parser')) {
 	class wpdef_text_parser {
-		const tooltip_html = '<a href="rldh_REPLACE_URL" class="rldh_definition" data-toggle="tooltip" title="rldh_REPLACE_TITLE">rldh_REPLACE_LINK</a>';
+		const tooltip_html = '<a href="{url}" class="wpdef-definition" data-toggle="tooltip" title="{tooltip}">{title}</a>';
 
 		public function __construct() {
 			add_action( 'wp_enqueue_scripts',
@@ -14,29 +14,37 @@ if (!class_exists('wpdef_text_parser')) {
 		}
 
 		public function enqueue_assets() {
-			wp_register_style( 'rldh-tooltipcss',
+			wp_register_style( 'wpdef-tooltipcss',
 				WPDEF_URL . 'css/tooltip.css' );
-			wp_enqueue_style( 'rldh-tooltipcss' );
-			wp_enqueue_script( "rldh-tooltipjs",
-				WPDEF_URL . "js/tooltip.js", array( 'jquery' ), '1.0.0',
-				true );
-			wp_enqueue_script( "rldh-js", WPDEF_URL . "js/main.js",
-				array( 'jquery' ), '1.0.0', true );
+			wp_enqueue_style( 'wpdef-tooltipcss' );
 		}
 
 
-		private function get_tooltip_link( $url, $link, $title ) {
+		/**
+		 * Return html with hyperlink and tooltip
+		 * @param string $url
+		 * @param string $tooltip
+		 * @param string $title
+		 *
+		 * @return string
+		 */
+
+		private function get_tooltip_link( $url, $tooltip, $title ) {
 			return apply_filters( 'rldh_tooltip_html', str_replace( array(
-				"rldh_REPLACE_URL",
-				"rldh_REPLACE_LINK",
-				"rldh_REPLACE_TITLE"
-			), array( $url, $link, $title ), self::tooltip_html ) );
+				"{url}",
+				"{tooltip}",
+				"{title}"
+			), array( $url, $tooltip, $title ), self::tooltip_html ) );
 		}
+
+		/**
+		 * @param string $content
+		 *
+		 * @return string
+		 */
 
 		public function replace_definitions_with_links( $content ) {
 			$count          = 0;
-			$content_length = strlen( $content );
-			$unlikely_nr    = $content_length + 10;
 
 			//find definitions in buffer
 			$args = array(
@@ -50,15 +58,12 @@ if (!class_exists('wpdef_text_parser')) {
 				$args ) );
 			if ( $definitions ) {
 				foreach ( $definitions as $definition ) {
-					_log( $definition );
+					_log($definition);
 					//check if this post IS this definition, else skip to next definition
 					if ( get_the_ID() != $definition->ID ) {
-						$start_looking_at = 0;
 						$url              = get_permalink( $definition->ID );
-						$name             = apply_filters( 'the_title',
-							$definition->post_title );
-						$excerpt          = apply_filters( 'the_excerpt',
-							$definition->post_excerpt );
+						$name             = apply_filters( 'the_title', $definition->post_title );
+						$excerpt          = apply_filters( 'the_excerpt', $definition->post_excerpt );
 						//remove tags from excerpt
 						$excerpt = strip_tags( $excerpt );
 						//remove quotes
@@ -67,21 +72,19 @@ if (!class_exists('wpdef_text_parser')) {
 						if ( strlen( $excerpt ) > 0 ) {
 							$excerpt = $excerpt . " ";
 						}
-						$excerpt .= __( 'Click for more information',
-							'definitions' );
+						$excerpt .= __( 'Click for more information', 'definitions' );
 
 						$excerpt_arr[]     = $excerpt;
 						$placeholder       = $count . '_rldh_excerpt';
 						$placeholder_arr[] = $placeholder;
 
-						$link = $this:: get_tooltip_link( $url, $name,
-							$placeholder );
+						$link = $this:: get_tooltip_link( $url, $name, $placeholder );
 
 						//continue until end of string, or break
 						// use regex instead https://stackoverflow.com/questions/958095/use-regex-to-find-specific-string-not-in-html-tag
 						//regex: replace $name in $content with $link
 
-						$pattern = '/(?![^<]*>)(wordpress)/i';
+						$pattern = '/(?![^<]*>)(' . $name . ')/i';
 						$content = preg_replace( $pattern, $link, $content );
 
 						$count ++;
