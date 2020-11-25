0e419ca72dbc451ec8dc6c5fd15b474cb4ec3245
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-11-25 14:32:24 +0100

    preview

--- text_parser.php
+++ text_parser.php
@@ -1,9 +1,10 @@
 <?php
-defined('ABSPATH') or die("you do not have acces to this page!");
+defined('ABSPATH') or die("you do not have access to this page!");
 
 if (!class_exists('wpdef_text_parser')) {
 	class wpdef_text_parser {
-		const tooltip_html = '<a href="{url}" class="wpdef-definition" data-toggle="tooltip" title="{tooltip}">{title}</a>';
+
+		public $tooltip_type = 'preview'; //or preview
 
 		public function __construct() {
 			add_action( 'wp_enqueue_scripts',
@@ -11,30 +12,91 @@ if (!class_exists('wpdef_text_parser')) {
 
 			add_filter( 'the_content',
 				array( $this, 'replace_definitions_with_links' ) );
+
+			add_action( 'wp_ajax_nopriv_wpdef_load_preview',
+				array( $this, 'wpdef_load_preview' ) );
+			add_action( 'wp_ajax_wpdef_load_preview',
+				array( $this, 'wpdef_load_preview' ) );
 		}
 
 		public function enqueue_assets() {
-			wp_register_style( 'wpdef-tooltipcss',
-				WPDEF_URL . 'css/tooltip.css' );
-			wp_enqueue_style( 'wpdef-tooltipcss' );
+			wp_register_style( 'wpdef-tooltip', WPDEF_URL . 'assets/css/tooltip.css' , array(), WPDEF_VERSION );
+			wp_enqueue_style( 'wpdef-tooltip' );
+
+			$minified = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? ''
+				: '.min';
+
+			wp_enqueue_script( 'wpdef',
+				WPDEF_URL . "assets/js/definitions$minified.js", array('jquery'),
+				WPDEF_VERSION, true );
+			wp_localize_script(
+				'wpdef',
+				'wpdef',
+				array(
+					'url'=> admin_url('admin-ajax.php'),
+				)
+			);
 		}
 
+		public function wpdef_load_preview(){
+			error_log("ajax call");
+			error_log(print_r($_GET, true));
+			$definitions_ids = array_map( 'intval' , $_GET['ids']);
+			error_log(print_r($definitions_ids, true));
+			//get_post();
+
+			$error          = false;
+			$previews = array();
+			foreach ($definitions_ids as $definitions_id ) {
+
+				$postid = $definitions_id;
+				$content_post = get_post($postid);
+				$content = $content_post->post_content;
+				$img = get_the_post_thumbnail($content_post, 'medium' );
+				$previews[] = array(
+					'id' => $definitions_id,
+					'html' => '<div class="wpdef-preview-content">'.$img.$content.'</div>',
+				);
+			}
+			
+			$response = array(
+				'success' => !$error,
+				'previews' => $previews,
+			);
+
+			$response = json_encode($response);
+			header( "Content-Type: application/json" );
+			echo $response;
+			exit;
+		}
 
 		/**
 		 * Return html with hyperlink and tooltip
 		 * @param string $url
 		 * @param string $tooltip
 		 * @param string $title
+		 * @param int $post_id
 		 *
 		 * @return string
 		 */
 
-		private function get_tooltip_link( $url, $tooltip, $title ) {
-			return apply_filters( 'rldh_tooltip_html', str_replace( array(
+		private function get_tooltip_link( $url, $tooltip, $title , $post_id) {
+			$classes[] = sanitize_title($title);
+			$classes[] = 'wpdef-definition';
+			$classes[] = 'wpdef-'.$this->tooltip_type;
+			$class = implode(' ', apply_filters( 'wpdef-classes', $classes) );
+			if ($this->tooltip_type === 'tooltip'){
+				$tooltip_html = '<a href="{url}" class="'.$class.'"><span data-hover="{tooltip}">{title}</span></a>';
+			} else {
+				$tooltip_html = '<dfn title="{title}" class="'.$class.'" data-definitions_id="{post_id}"></dfn>';
+				// https://stackoverflow.com/questions/40531029/how-to-create-a-pure-css-tooltip-with-html-content-for-inline-elements
+			}
+			return apply_filters( 'wpdef_tooltip_html', str_replace( array(
 				"{url}",
 				"{tooltip}",
-				"{title}"
-			), array( $url, $tooltip, $title ), self::tooltip_html ) );
+				"{title}",
+				"{post_id}"
+			), array( $url, $tooltip, $title , $post_id), $tooltip_html ) );
 		}
 
 		/**
@@ -44,7 +106,7 @@ if (!class_exists('wpdef_text_parser')) {
 		 */
 
 		public function replace_definitions_with_links( $content ) {
-			$count          = 0;
+			$count = 0;
 
 			//find definitions in buffer
 			$args = array(
@@ -58,7 +120,6 @@ if (!class_exists('wpdef_text_parser')) {
 				$args ) );
 			if ( $definitions ) {
 				foreach ( $definitions as $definition ) {
-					_log($definition);
 					//check if this post IS this definition, else skip to next definition
 					if ( get_the_ID() != $definition->ID ) {
 						$url              = get_permalink( $definition->ID );
@@ -72,20 +133,23 @@ if (!class_exists('wpdef_text_parser')) {
 						if ( strlen( $excerpt ) > 0 ) {
 							$excerpt = $excerpt . " ";
 						}
-						$excerpt .= __( 'Click for more information', 'definitions' );
+						$excerpt .= __( 'Click for more information', 'definitions');
 
 						$excerpt_arr[]     = $excerpt;
 						$placeholder       = $count . '_rldh_excerpt';
 						$placeholder_arr[] = $placeholder;
-
-						$link = $this:: get_tooltip_link( $url, $name, $placeholder );
+						$tooltip = $excerpt;
+						$link = $this::get_tooltip_link( $url, $tooltip, $name , $definition->ID);
 
 						//continue until end of string, or break
 						// use regex instead https://stackoverflow.com/questions/958095/use-regex-to-find-specific-string-not-in-html-tag
 						//regex: replace $name in $content with $link
 
 						$pattern = '/(?![^<]*>)(' . $name . ')/i';
-						$content = preg_replace( $pattern, $link, $content );
+						$limit = 10; //how many times a found definition can be replaced
+
+
+						$content = preg_replace( $pattern, $link, $content, $limit );
 
 						$count ++;
 					}
